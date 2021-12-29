ee0a0ec736d9fa6f6cd5ff490757b6ec2e913c73
zhixuanlee <overlypositiveplayer@gmail.com>
2021-12-29 20:41:51 +0800

    Set up EnigmaM4

---
+++ src/enigma/EnigmaM4.java
@@ -0,0 +1,36 @@
+package enigma;
+
+public class EnigmaM4 extends EnigmaM3{
+
+    public Rotor fourthRotor;
+
+    public EnigmaM4(String[] rotorNames, String reflector, int[] rotorOffsets, int[] ringSettings, String plugboardConnections) {
+        super(rotorNames, reflector, rotorOffsets, ringSettings, plugboardConnections);
+        this.fourthRotor = Rotor.createRotor(rotorNames[3], rotorOffsets[3], ringSettings[3]);
+    }
+
+    @Override
+    public int encrypt(int input) {
+        rotate();
+
+        int newInput = this.plugBoard.goForward(input);
+
+        // newInput will go through a series of 9 mappings at the rotors
+
+        int firstChange = firstRotor.goForward(newInput);
+        int secondChange = secondRotor.goForward(firstChange);
+        int thirdChange = thirdRotor.goForward(secondChange);
+        int fourthChange = fourthRotor.goForward(thirdChange);
+
+        int fifthChange = reflector.goForward(fourthChange);
+
+        int sixthChange = fourthRotor.goBackward(fifthChange);
+        int seventhChange = thirdRotor.goBackward(sixthChange);
+        int eigthChange = secondRotor.goBackward(seventhChange);
+        int ninthChange = firstRotor.goBackward(eigthChange);
+
+        int finalResult = plugBoard.goForward(ninthChange);
+
+        return finalResult;
+    }
+}
