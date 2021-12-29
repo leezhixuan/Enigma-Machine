e9b181dca29dfd9cf8651daadd9bc079ab0357dd
zhixuanlee <overlypositiveplayer@gmail.com>
2021-12-29 15:33:17 +0800

    Complete Reflector

---
+++ src/enigma/Reflector.java
@@ -0,0 +1,36 @@
+package enigma;
+
+import javax.management.relation.Relation;
+
+public class Reflector {
+    protected String name;
+    protected int[] forwardWires;
+
+    public Reflector(String name, String mapping) {
+        this.name = name;
+        this.forwardWires = decodeWiring(mapping);
+    }
+
+    protected static Reflector createReflector(String name) {
+        switch (name) {
+            case "B":
+                return new Reflector("B", "YRUHQSLDPXNGOKMIEBFZCWVJAT");
+            case "C":
+                return new Reflector("C", "FVPJIAOYEDRZXWGCTKUQSBNMHL");
+        }
+        return null;
+    }
+
+    public int[] decodeWiring(String mapping) {
+        char[] charArray = mapping.toCharArray();
+        int[] wiring = new int[charArray.length];
+        for (int i = 0; i < charArray.length; i++) {
+            wiring[i] = charArray[i] - 65; // 65 is the ascii value of 'a'
+        }
+        return wiring;
+    }
+
+    public int goForward(int input) {
+        return this.forwardWires[input];
+    }
+}
