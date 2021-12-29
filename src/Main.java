ae2748f2f68f3b63ce352c8866168f31e736df03
zhixuanlee <overlypositiveplayer@gmail.com>
2021-12-30 03:59:42 +0800

    Demo of EnigmaM3

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
+import enigma.EnigmaM3;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        String[] rotorNamesM3 = new String[]{"III", "II", "I"};
+        int[] rotorOffsetsM3 = new int[]{0, 0, 0};
+        int[] ringSettingsM3 = new int[]{0, 0, 0};
+        EnigmaM3 enigmaM3 = new EnigmaM3(rotorNamesM3, "B", rotorOffsetsM3, ringSettingsM3, "");
+        String input = "HELLO";
+        System.out.println(enigmaM3.encrypt(input));
+
+
     }
 }
